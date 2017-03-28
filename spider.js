a7e0ab4850b233d155e3ef9bc3440d28b0c9ef2a
silvanoamaral <silvano.amaral9@gmail.com>
2017-03-27 21:51:52 -0300

    Publicando arquivo package.json

--- spider.js
+++ spider.js
@@ -1,13 +1,19 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');//Salvar os dados em uma pasta (node)
+var express = require('express'),
+    fs = require('fs'),//Salvar os dados em uma pasta (node)
+    request = require('request'),//Para fazer chamadas HTTP
+    cheerio = require('cheerio'),//Para acessar o DOM externo e extrair os dados
+    app = express();
 
 var url = 'https://www.hering.com.br/store/';
 
 request(url, function(error,response,html){
-	if(error) console.log('[Error] '+ error);
+	if(!error && response.statusCode === 200){
+		var $ = cheerio.load(html);
 
-	var $ = cheerio.load(html);
+		var corpo = $(html).find('body');
 
-	fs.appendFile('home.html',html);
+		fs.appendFile('home.html',corpo);
+	}else{
+		console.error('[] '+err);
+	}
 });
